64d37d39e12e1adf9414c0704b2ac0085042e6b4
freedom-pep <freedom-pep@proton.me>
2025-04-23 15:58:51 -0500

    Set simplenote keystroke with capital S

--- lua/custom/plugins/simplenote.lua
+++ lua/custom/plugins/simplenote.lua
@@ -2,11 +2,11 @@ return {
   'simplenote-vim/simplenote.vim',
 
   config = function()
-    vim.keymap.set('n', '<leader>ml', vim.cmd.SimplenoteList, { desc = 'List simplenote' })
-    vim.keymap.set('n', '<leader>mn', vim.cmd.SimplenoteNew, { desc = 'New simplenote' })
-    vim.keymap.set('n', '<leader>mp', vim.cmd.SimplenotePin, { desc = 'Pin simplenote' })
-    vim.keymap.set('n', '<leader>md', vim.cmd.SimplenoteTrash, { desc = 'Delete simplenote' })
-    vim.keymap.set('n', '<leader>mu', vim.cmd.SimplenoteUpdate, { desc = 'Upadete simplenote' })
+    vim.keymap.set('n', '<leader>Sl', vim.cmd.SimplenoteList, { desc = 'List simplenote' })
+    vim.keymap.set('n', '<leader>Sn', vim.cmd.SimplenoteNew, { desc = 'New simplenote' })
+    vim.keymap.set('n', '<leader>Sp', vim.cmd.SimplenotePin, { desc = 'Pin simplenote' })
+    vim.keymap.set('n', '<leader>Sd', vim.cmd.SimplenoteTrash, { desc = 'Delete simplenote' })
+    vim.keymap.set('n', '<leader>Su', vim.cmd.SimplenoteUpdate, { desc = 'Upadete simplenote' })
 
     vim.g.SimplenoteUsername = 'upjohnc@pm.me'
     vim.g.SimplenotePassword = '$SN_PASSWORD'
